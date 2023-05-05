13ffe6e84c51870585c4e2a8e977dc4c87ebca73
Kisusyenni Venessa <kisusyenni01@gmail.com>
2023-05-05 21:44:44 +0700

    Remove token

--- app/src/main/java/com/dicoding/githubuserapp/network/ApiConfig.kt
+++ app/src/main/java/com/dicoding/githubuserapp/network/ApiConfig.kt
@@ -11,19 +11,19 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
     companion object{
         fun getApiService(): ApiService {
-//            val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-//            val client = OkHttpClient.Builder()
-//                .addInterceptor(loggingInterceptor)
-//                .build()
+            val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
 
 //            With Github Token
-            val token = "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" // add your github token
-            val client = OkHttpClient.Builder().addInterceptor(Interceptor { chain ->
-                val newRequest: Request = chain.request().newBuilder()
-                    .addHeader("Authorization", "Bearer $token")
-                    .build()
-                chain.proceed(newRequest)
-            }).build()
+//            val token = "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" // add your github token
+//            val client = OkHttpClient.Builder().addInterceptor(Interceptor { chain ->
+//                val newRequest: Request = chain.request().newBuilder()
+//                    .addHeader("Authorization", "Bearer $token")
+//                    .build()
+//                chain.proceed(newRequest)
+//            }).build()
 
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
