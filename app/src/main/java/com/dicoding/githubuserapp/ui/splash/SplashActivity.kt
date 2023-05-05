d2b37c43b8af39abda7241f6466b21ec5f649021
Kisusyenni Venessa <kisusyenni01@gmail.com>
2023-05-05 21:45:38 +0700

    Add timer to SplashActivity.kt

--- app/src/main/java/com/dicoding/githubuserapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/dicoding/githubuserapp/ui/splash/SplashActivity.kt
@@ -2,6 +2,8 @@ package com.dicoding.githubuserapp.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.githubuserapp.ui.main.MainActivity
 
@@ -9,8 +11,11 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val splashIntent = Intent(this@SplashActivity, MainActivity::class.java)
-        startActivity(splashIntent)
-        finish()
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            val splashIntent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(splashIntent)
+            finish()
+        }, 3000)
     }
 }
